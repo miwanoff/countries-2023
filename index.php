2cc228a532b373bf28ac2073cdad8ca2448ebe9f
miwanoff <marina.iva.noffa@gmail.com>
2023-11-04 16:03:47 +0200

    Add template

--- index.php
+++ index.php
@@ -20,29 +20,61 @@ if (isset($_POST["go"])) {
     }
 }
 $user_form = '<form action="' . $_SERVER['PHP_SELF'] . '" method="post" name="autoForm">
-Логин: <input type="text" name="login">
-Пароль: <input type="password" name="pass">
-<input type="submit" value="Подтвердить" name="go">
+<input type="text" name="login" placeholder="Input login">
+<input type="password" name="pass" placeholder="Input password">
+<input type="submit" value="Go" name="go">
 </form>';
 
 if (!$autorized) {
     echo $user_form;
 }
 
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="page-content">
+                <!-- ***** Banner Start ***** -->
+                <div class="main-banner">
+                    <div class="row">
+                        <div class="col-lg-7">
+                            <div class="header-text">
+                                <h6>Welcome To Cyborg</h6>
+                                <h4><em>Browse</em> Our Popular Games Here</h4>
+                                <div class="main-button">
+                                    <a href="browse.html">Browse Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ***** Banner End ***** -->
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- ***** Most Popular Start ***** -->
+<div class="most-popular">
+    <div class="row">
+        <div class="col-lg-12">
+            <?php
+
 $str_form_s = '<h3>Сортировать по:</h3>
 <form action="index.php" method="post" name="sort_form">
-<select name="sort" id="sort" size="1">
-    <option value="name">названию</option>
-    <option value="area">площади</option>
-    <option value="population">среднему населению</option>
-</select>
-<input type="submit" name="submit" value="OK">
+    <select name="sort" id="sort" size="1">
+        <option value="name">названию</option>
+        <option value="area">площади</option>
+        <option value="population">среднему населению</option>
+    </select>
+    <input type="submit" name="submit" value="OK">
 </form>';
 echo $str_form_s;
 
 if (isset($_POST["sort"])) {
     $how_to_sort = $_POST["sort"];
     sorting($how_to_sort);
+
     $out = out_arr();
 
     if (count($out) > 0) {
@@ -56,12 +88,12 @@ if (isset($_POST["sort"])) {
 
 $str_form_search = "
 <div class=\"container\">
-  <h3>Search:</h3>
-	<form  name='searchForm' action='index.php' method='post' onSubmit='return overify_login(this);' >
- 		<input type='text' name='search' class='form-control' >
- 		<input type='submit' name='gosearch' value='Confirm'  class='btn btn-secondary my-2'>
- 		<input type='reset' name='clear' value='Reset'  class='btn btn-secondary my-2'>
- 	</form>
+    <h3>Search:</h3>
+    <form name='searchForm' action='index.php' method='post' onSubmit='return overify_login(this);'>
+        <input type='text' name='search' class='form-control'>
+        <input type='submit' name='gosearch' value='Confirm' class='btn btn-secondary my-2'>
+        <input type='reset' name='clear' value='Reset' class='btn btn-secondary my-2'>
+    </form>
 </div>";
 
 echo $str_form_search;
@@ -80,5 +112,10 @@ if (isset($_POST['gosearch'])) {
         echo "Nothing found...";
     }
 }
-
-include "footer.php";
+?>
+        </div>
+    </div>
+</div>
+<!-- ***** Most Popular End ***** -->
+<?php           
+            include "footer.php";
